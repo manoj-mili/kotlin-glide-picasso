b50340cbf1a90f4652e9038c36b3700260442aa8
Manoj Mohanty <iammanojmj@gmail.com>
2019-11-28 03:01:15 +0530

    Update Constants.kt

--- app/src/main/java/com/mili/glideandpicasso/util/Constants.kt
+++ app/src/main/java/com/mili/glideandpicasso/util/Constants.kt
@@ -2,5 +2,5 @@ package com.mili.glideandpicasso.util
 
 object Constants {
     const val BASE_URL = "https://pixabay.com/";
-    const val API_KEY = "10222648-d15f4544086ecc2cf54ceb299";
+    const val API_KEY = "ADD YOUR KEY HERE";
 }
